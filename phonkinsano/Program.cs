4c5a89d43618bdff0cf2aa85ab268633e5df67fe
Emilio <100852610+EmilioAnshe@users.noreply.github.com>
2024-08-01 22:17:15 -0300

    Add files via upload

---
+++ phonkinsano/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace MiLibreria{
+
+    abstract class Persona{
+        public string nombre;
+
+        public int dni;
+
+        public void caminar(){
+            System.Console.WriteLine("Esta caminando");
+        }
+
+        public string correr(){
+            return "esta corriendoooo";
+        }
+
+         public abstract void llorar();
+        }
+       
+
+    class Alumno : Persona{
+            public int legajo;
+            public int dni;
+
+            public void rendir(){
+                System.Console.WriteLine("esta rindiendo...");
+            }
+        public override void llorar()
+        {
+            System.Console.WriteLine("el alumno esta llorando");
+        }
+    }
+  
+    class Profesor : Persona{
+                public int sueldo;
+                public void tomarExamen(){
+
+                }
+        public override void llorar()
+        {
+            System.Console.WriteLine("El profe esta llorando");
+        }
+    }
+            }
+        
+            
+        
+    
---
+++ phonkinsano/principal.cs
@@ -0,0 +1,18 @@
+using MiLibreria;
+
+class principal{
+    static void Main(){
+
+        Alumno Emilio = new Alumno();
+
+        Profesor Ivan = new Profesor();
+
+        Emilio.llorar();
+        Emilio.caminar();
+        Ivan.llorar();
+        
+
+
+    }
+}
+
